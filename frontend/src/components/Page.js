418d05e8a84af3e39cf62dba749b7753693f4e1a
SpectralGT <atharvsingh1396@gmail.com>
2023-07-24 16:44:06 +0530

    created functionality to update the app state

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -8,10 +8,20 @@ function Page() {
 
   return (
     <div className="page">
-      {data.map((e, id) => {
+      {data.map((e, index) => {
         if (e[0] === "p") {
           return (
-            <div key={id} className="para" contentEditable>
+            <div
+              key={index}
+              className="para"
+              onKeyUp={(event) => {
+                var new_data = data;
+                new_data.splice(index, 1, event.currentTarget.innerText);
+                console.log(new_data);
+                setData(new_data);
+              }}
+              contentEditable
+            >
               {e[1]}
             </div>
           );
